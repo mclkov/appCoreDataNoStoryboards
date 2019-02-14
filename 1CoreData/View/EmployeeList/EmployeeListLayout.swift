b4e9d34ef8b30427dd09fdc0bf180db8a3f5b1b1
McL <mcl@brm>
2019-02-14 21:19:07 +0200

    more obvious naming of view files

---
+++ 1CoreData/View/EmployeeList/EmployeeListLayout.swift
@@ -0,0 +1,13 @@
+//
+//  ListLayout.swift
+//  1CoreData
+//
+//  Created by McL on 2/14/19.
+//  Copyright © 2019 McL. All rights reserved.
+//
+
+import UIKit
+
+extension EmployeeListVC {
+    
+}
